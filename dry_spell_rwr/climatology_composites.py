Fix calculation of RWR when there are masked data (closes #1).
The RWR linear regression would typically see y=0.0 on the masked days.

dry_spell_rwr/climatology_composites.py
@@ -7,7 +7,7 @@ from __future__ import print_function, division
 
 import numpy as np
 from cftime import utime
-from scipy.stats import linregress
+from scipy.stats.mstats import linregress
 
 from . import (FMDI, _RWR_BEG, _RWR_END, _NDAYS_ANTE, _NDAYS_DRY,
                _PTILES, _WIN_LEN, logger)
@@ -330,19 +330,26 @@ class Composite(object):
     def calc_rwr(self, days):
         """Calculate RWR stats over some sub-period of the composite.
 
+        RWR stats are calculated if there are at least two days with data.
+
         Parameters
         ----------
         days : slice
             Subperiod over which RWR is calculated.
         """
 
-        if self.bounds is None:
-            self.stats = [FMDI, ]*5
-        else:
+        self.stats = [FMDI, ]*5
+
+        if self.bounds is not None:
             days_c = self.get_days()
-            self.stats = linregress(days_c[days], self.td[days])
+
+            x, y = days_c[days], self.td[days]
+            if np.ma.count(y) > 1:
+                self.stats = linregress(x, y)
+
             self.regress_day_start = days_c[days][0]
             self.regress_day_end = days_c[days][-1]
+
         return
 
 

dry_spell_rwr/file_io.py
@@ -9,7 +9,7 @@ from __future__ import print_function, division
 import netCDF4 as nc
 import numpy as np
 from cftime import utime
-from scipy.stats import linregress
+from scipy.stats.mstats import linregress
 
 from . import FMDI, logger
 from .climatology_composites import Composite
@@ -406,14 +406,17 @@ def nc_rwr_combine(grid_file, files_in, file_out):
     # Finalise the weighted composite mean.
     td_all /= wsum_all
 
-    # Calculate stats on the new combined TD.
+    # Calculate stats on the new combined TD if there is more than one day with
+    # data.
     def calc_rwr(td):
-        if all(td.mask):
-            stats = [FMDI, ] * 5
+        s = slice(ndays_ante + regress_day_start,
+                  ndays_ante + regress_day_end + 1)
+        x, y = days[s], td[s]
+        if np.ma.count(y) > 1:
+            stats = linregress(x, y)
         else:
-            s = slice(ndays_ante + regress_day_start,
-                      ndays_ante + regress_day_end + 1)
-            stats = linregress(days[s], td[s])
+            stats = [FMDI, ] * 5
+
         return stats
 
     stats_out = [s for s in np.apply_along_axis(calc_rwr, 0, td_all)]